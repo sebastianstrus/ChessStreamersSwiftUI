65c1c2982823731ffbb0fb8567720c43fd7ef2d2
sebastianstrus <sebastian.strus1@gmail.com>
2022-08-23 11:56:43 +0200

    Initial Commit

---
+++ ChessStreamersSwiftUI/ChessStreamersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessStreamersSwiftUIApp.swift
+//  ChessStreamersSwiftUI
+//
+//  Created by Sebastian Strus on 2022-08-23.
+//
+
+import SwiftUI
+
+@main
+struct ChessStreamersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
