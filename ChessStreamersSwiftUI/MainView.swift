bed4db82d42f2a2194d06a46a867c39332ded5f4
sebastianstrus <sebastian.strus1@gmail.com>
2022-09-02 01:27:54 +0200

    wip

--- ChessStreamersSwiftUI/MainView.swift
+++ ChessStreamersSwiftUI/MainView.swift
@@ -18,21 +18,25 @@ struct MainView: View {
                                     count: Device.IS_IPAD ? 8 : 4)
 
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(streamers, id: \.self) { item in
-                        NavigationLink(destination: DetailView(streamer: item)) {
-                            AsyncImage(url: URL(string: item.avatarUrl ?? ""),
-                                       content: { image in
-                                image.resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            }, placeholder: {
-                                ProgressView()
-                            })
+        print("Streamers fetched from internet: \(model.streamers.count)")
+        print("Streamers saved in Core Data: \(streamers.count)")
+        return NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(model.streamers, id: \.self) { item in
+                            NavigationLink(destination: DetailView(streamer: item)) {
+                                AsyncImage(url: URL(string: item.avatarUrl ?? ""),
+                                           content: { image in
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                }, placeholder: {
+                                    ProgressView()
+                                })
+                            }
                         }
                     }
-                }
+                }.navigationTitle("Chess Streamers")
                 
                 
                 Button("Add") {
@@ -40,12 +44,11 @@ struct MainView: View {
                     streamer.id = UUID()
                     streamer.username = "Sebastian"
                     streamer.isLive = true
-                    streamer.avatarUrl = "https://scontent-arn2-2.xx.fbcdn.net/v/t31.18172-8/11700821_10207249342662960_1428027601653512940_o.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=jiTWFXA5IUgAX8krDCj&_nc_ht=scontent-arn2-2.xx&oh=00_AT93DrCrwIYhszF2PKYVyxuzih-Z3XPPKdBEseH0cThz3A&oe=6334EDE2"
                     streamer.url = "https://www.facebook.com/sebastian.strus"
+                    streamer.avatarUrl = "https://sebastianstrus.github.io/img/portrait.jpg"
                     try? moc.save()
                 }
-                
-            }.navigationTitle("Chess Streamers")
+            }
             
         }.navigationViewStyle(StackNavigationViewStyle())
     }
